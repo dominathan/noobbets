34e6e51a1588d0734020d2cccf9ca8b0e6ecd26d
Nathan Hall <nathan.mh@gmail.com>
2015-05-26 17:41:06 -0400

    added bets controller and routes, redirect to bets on sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,23 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    bets_path || root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    bets_path || root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path || root_path
+  end
+
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+      devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+    end
 end
---
+++ app/controllers/bets_controller.rb
@@ -0,0 +1,6 @@
+class BetsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+end
