dc4c650f3c980df580db4a9f28748393ba672482
Nathan Hall <nathan.mh@gmail.com>
2015-06-05 18:52:41 -0400

    comment clarification

--- app/models/lolteam.rb
+++ app/models/lolteam.rb
@@ -8,8 +8,8 @@ class Lolteam < ActiveRecord::Base
   before_save :max_entrants_limit_reached?, :unique_summoners_per_lolteam?, :bet_already_started?,
               :user_has_enough_fake_money?
 
-  # Returns the an array of each indiviudal summoner on a single lolteam with id, ending with total
-  # E.G. [[179.62, 1965], [555.11, 2136], [550.29, 2276], [198.58, 2251], [308.65, 2367], [143.35, 3477], [265.99, 4017], [2201.59, nil]]
+  # Returns the an object of all 7 summoners on a single lolteam with id, name, score,
+  # Ending with total_score for all summoners
   def score_user_lolteam
     lolteam = self
     summoner_ids = lolteam.attributes.values.slice(3,7)
