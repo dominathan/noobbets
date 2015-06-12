43b93e6a50bb4791ff4a0a8878037de3765625bb
Nathan Hall <nathan.mh@gmail.com>
2015-06-12 14:09:17 -0400

    begin working on jobs to handle payout

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Noobbets
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.active_job.queue_adapter = :sidekiq
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-
-
-
   root 'homes#index'
 
   devise_for :users
