2702f3bbe89e6b73937772b86bc1f35d286f1f87
Nathan Hall <nathan.mh@gmail.com>
2015-06-21 11:32:26 -0400

    added basic test for scoresummoner

--- spec/jobs/summoner_score_job_spec.rb
+++ spec/jobs/summoner_score_job_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SummonerScoreJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:summoner1) {FactoryGirl.create(:summoner) }
+  let(:summoner2) {FactoryGirl.create(:summoner) }
+  context 'queueing the job' do
+    it 'should change the the number of jobs' do
+      Sidekiq::Testing.fake!
+      expect {
+        SummonerScoreJob.perform_async }.to change(
+          SummonerScoreJob.jobs, :size).by(1)
+    end
+  end
+
 end
