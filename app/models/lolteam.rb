ae4461ef4aa2ee2303527a95e60636ed9d5e8309
Nathan Hall <nathan.mh@gmail.com>
2015-06-21 16:32:05 -0400

    git status

--- app/models/lolteam.rb
+++ app/models/lolteam.rb
@@ -8,7 +8,6 @@ class Lolteam < ActiveRecord::Base
   validate :unique_summoners_per_lolteam?, :max_entrants_limit_reached?, :bet_already_started?,
            :user_has_enough_fake_money?
 
-
   # Returns the an object of all 7 summoners on a single lolteam with id, name, score,
   # Ending with total_score for all summoners
   def score_user_lolteam
@@ -20,7 +19,7 @@ class Lolteam < ActiveRecord::Base
       scoring_object["id#{idx + 1}"] = summoner.id
       scoring_object["name#{idx + 1}"] = summoner.name
     end
-    scoring_object['total_score'] = scoring_object['slot1'] + scoring_object['slot2'] + scoring_object['slot3'] + scoring_object['slot4'] + scoring_object['slot5'] + scoring_object['slot6'] + scoring_object['slot7']
+    scoring_object['total_score'] = scoring_object['slot1'].round(2) + scoring_object['slot2'].round(2) + scoring_object['slot3'].round(2) + scoring_object['slot4'].round(2) + scoring_object['slot5'].round(2) + scoring_object['slot6'].round(2) + scoring_object['slot7'].round(2)
     scoring_object[:user_id] = self.user.id
     scoring_object[:lolteam_id] = self.id
     scoring_object
